fcdd41f55f0b281fa2352d0e9d50e59d4fcfc0c1
Wackery <32279125+webmsgr@users.noreply.github.com>
2019-09-30 12:32:51 -0700

    Update ohol.pyx

--- ohol.pyx
+++ ohol.pyx
@@ -1,7 +1,7 @@
 """A One Hour One Life decoder/encoder server, recives messages from the client and 
 passes them to bot code that allows for packet injection and packet reading"""
-cpdef enum messageType {
-	MOVE,
+ctypedef enum messageType:
+    MOVE,
     USE,
     SELF,
     BABY,
@@ -30,4 +30,18 @@ cpdef enum messageType {
     VOGX,
     PHOTO,
     UNKNOWN
-    } messageType;
+cdef struct ClientMessage:
+        messageType type
+        int x, y, c, i, id
+        int trigger
+        int bug
+        # some messages have extra positions attached
+        int numExtraPos
+        #NULL if there are no extra
+        GridPos *extraPos
+        # null if type not SAY
+        char *saidText       
+        # null if type not BUG
+        char *bugText
+        # for MOVE messages
+        int sequenceNumber
