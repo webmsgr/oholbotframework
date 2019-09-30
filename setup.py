30d02f277c5397840bc8bf1b861bc8d4b472723c
Wackery <32279125+webmsgr@users.noreply.github.com>
2019-09-30 12:47:59 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ from distutils.core import setup
 from Cython.Build import cythonize
 
 setup(
-    ext_modules = cythonize("ohol.pyx",languge_level=3)
+    ext_modules = cythonize("ohol.pyx",compiler_directives={'language_level' : "3"})
 )
