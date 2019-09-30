a369a24e0b97eaee77ef09242cfb007b8eab5817
Wackery <32279125+webmsgr@users.noreply.github.com>
2019-09-30 12:42:47 -0700

    Update ohol.pyx

--- ohol.pyx
+++ ohol.pyx
@@ -1,6 +1,7 @@
 """A One Hour One Life decoder/encoder server, recives messages from the client and 
 passes them to bot code that allows for packet injection and packet reading"""
 
+
 cdef struct s_GridPos:
     int x
     int y
