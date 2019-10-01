fdc7a824208cd363cb7001b43cc58c96d5d57ff3
Wackery <zapcontact000@gmail.com>
2019-10-01 16:25:53 +0000

    Parsed in server

--- ohol.py
+++ ohol.py
@@ -3,20 +3,20 @@
 passes them to bot code that allows for packet injection and packet reading"""
 
 import select
-
+import parser
 import socket
-
 import time
 
 
-DEF TIME_WAIT = 0.01
-DEF BIND_ADDR = ''
-DEF BIND_PORT = 8006
-DEF SERV_ADDR = 'server1.onehouronelife.com'
-DEF SERV_PORT = 8005
+TIME_WAIT = 0.01
+BIND_ADDR = ''
+BIND_PORT = 8006
+SERV_ADDR = 'server1.onehouronelife.com'
+SERV_PORT = 8005
 
 
 def Route():
+    myparser = parser.Parser()
     listener = socket.socket()
     listener.bind((BIND_ADDR, BIND_PORT))
     listener.listen(1)
@@ -29,12 +29,24 @@ def Route():
         try:
             rlist = select.select([client, server], [], [])[0]
             if client in rlist:
-                buf = client.recv(4096)
+                stream = b""
+                buf = b" "
+                while buf != b"":
+                    buf = client.recv(4096)
+                    stream += buf
+                buf = stream
+                myparser.parsepacket(buf.decode("ascii"),"s")
                 if len(buf) == 0:
                     running = False
                 server.send(buf)
             if server in rlist and running:
-                buf = server.recv(4096)
+                stream = b""
+                buf = b" "
+                while buf != b"":
+                    buf = server.recv(4096)
+                    stream += buf
+                buf = stream
+                myparser.parsepacket(buf.decode("ascii"),"c")
                 if len(buf) == 0:
                     running = False
                 client.send(buf)
@@ -48,5 +60,6 @@ def Route():
         server.close()
     except:
         pass
+    return myparser.parsedserver, myparser.parsedclient
 
 
--- parser.py
+++ parser.py
@@ -5,16 +5,38 @@ class Parser():
         self.clientfeed = []
         self.parsedserver = []
         self.parsedclient = []
-class BasePacket():
-    def __init__(self,direction)
+    def parsepacket(self,packet,direction):
+        packets = packet.split("#")
+        if packets[0] == packet:
+            packet = packet.split("\n")
+            packet = [x for x in packet if x.strip() != ""]
+            packetobj = packobj.get(packet[0],UnknownPacket)()
+            packetobj.parse(packet)
+            if direction == "s":
+                self.parsedserver.append(packetobj)
+            else:
+                self.parsedclient.append(packetobj)
+        else:
+            [self.parsepacket(x,direction) for x in packets if x.strip() != ""]
+class BasePacket:
+    def __init__(self,direction):
         self.direct = direction
+        self.type = "BASE"
     def parse(self,args): # takes the raw packet and populates itself
         pass
     def packet(self): # takes self and converts it to a packet
         pass
+class UnknownPacket(BasePacket):
+    def __init__(self):
+        super().__init__("?")
+        self.type = "UNKNOWN"
+    def parse(self,args):
+        self.data = args
+
 class Frame(BasePacket):
     def __init__(self):
         super().__init__("c") # send to client
+        self.type = "FRAME"
     def packet(self):
         return "FM\n#"
 packobj = {"FM":Frame}
