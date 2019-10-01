c9c44f09fb841c07c4bffe2674ff49ca9039903c
Wackery <32279125+webmsgr@users.noreply.github.com>
2019-10-01 08:23:20 -0700

    Update parser.py

--- parser.py
+++ parser.py
@@ -1,1 +1,8 @@
 """Parse client/server packets into a object and allow converting from the object to a packet"""
+
+class Parser():
+    def __init__(self):
+        self.serverfeed = []
+        self.clientfeed = []
+        self.parsedserver = []
+        self.parsedclient = []
