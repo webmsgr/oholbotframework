aa27a14b35d152a4c244f0bcb7ad1e4afdf8dd89
Wackery <32279125+webmsgr@users.noreply.github.com>
2019-10-01 07:43:49 -0700

    Update ohol.pyx

--- ohol.pyx
+++ ohol.pyx
@@ -97,39 +97,36 @@ DEF SERV_PORT = 8005
 
 
 def Route():
-    listener = socket.socket()
-    listener.bind((BIND_ADDR, BIND_PORT))
-    listener.listen(1)
-    client, caddr = listener.accept()
-    listener.close()
-    server = sock.socket()
-    server.connect((SERV_ADDR, SERV_PORT))
-    running = True
-    while running:
-        try:
-            rlist = select.select([client, server], [], [])[0]
-            if client in rlist:
-                buf = client.recv(4096)
-                if len(buf) == 0:
-                    running = False
-                # Parse, modify, or halt traffic here
-                server.send(buf)
-            if server in rlist and running:
-                buf = server.recv(4096)
-                if len(buf) == 0:
-                    running = False
-                # Parse, modify, or halt traffic here
-                client.send(buf)
-    except:
-        pass
-    try:
-        client.close()
-    except:
-        pass
-    try:
-
-        server.close()
-    except:
-        pass
+    listener = socket.socket()
+    listener.bind((BIND_ADDR, BIND_PORT))
+    listener.listen(1)
+    client, caddr = listener.accept()
+    listener.close()
+    server = sock.socket()
+    server.connect((SERV_ADDR, SERV_PORT))
+    running = True
+    while running:
+        try:
+            rlist = select.select([client, server], [], [])[0]
+            if client in rlist:
+                buf = client.recv(4096)
+                if len(buf) == 0:
+                    running = False
+                server.send(buf)
+            if server in rlist and running:
+                buf = server.recv(4096)
+                if len(buf) == 0:
+                    running = False
+                client.send(buf)
+    except:
+        pass
+    try:
+        client.close()
+    except:
+        pass
+    try:
+        server.close()
+    except:
+        pass
 
 
