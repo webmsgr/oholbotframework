ed33332a7bdfb60d0d5d8d71d24d426d8d792da9
Wackery <32279125+webmsgr@users.noreply.github.com>
2019-10-01 08:49:06 -0700

    Update parser.py

--- parser.py
+++ parser.py
@@ -1,5 +1,4 @@
 """Parse client/server packets into a object and allow converting from the object to a packet"""
-
 class Parser():
     def __init__(self):
         self.serverfeed = []
@@ -7,10 +6,15 @@ class Parser():
         self.parsedserver = []
         self.parsedclient = []
 class BasePacket():
-    def __init__(self,rawpacket,direction)
-        self.rawpacket = rawpacket
+    def __init__(self,direction)
         self.direct = direction
+    def parse(self,args): # takes the raw packet and populates itself
+        pass
+    def packet(self): # takes self and converts it to a packet
+        pass
 class Frame(BasePacket):
     def __init__(self):
-        super().__init__("FM\n#","c") # send to client
-        
+        super().__init__("c") # send to client
+    def packet(self):
+        return "FM\n#"
+packobj = {"FM":Frame}
