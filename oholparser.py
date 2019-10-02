e727b4ac74f35d4604bf31fff206bb8e06d88efb
Wackery <32279125+webmsgr@users.noreply.github.com>
2019-10-02 08:16:18 -0700

    Update oholparser.py

--- oholparser.py
+++ oholparser.py
@@ -21,8 +21,9 @@ class BasePacket:
     def __init__(self,direction):
         self.direct = direction
         self.type = "BASE"
-    def parse(self,args): # takes the raw packet and populates itself
-        pass
+        self.data = ""
+    def parse(self,args,raw): # takes the raw packet and populates itself
+        self.data = raw
     def packet(self): # takes self and converts it to a packet
         pass
 class UnknownPacket(BasePacket):
@@ -42,50 +43,51 @@ class Shutdown(BasePacket):
     def __init__(self):
         super().__init__("c")
         self.type = "SHUTDOWN"
-    def parse(self,data,_):
+    def parse(self,data,raw):
         self.playercount = data[1]
-        self.data = data
+        self.data = raw
 class Server_full(BasePacket):
     def __init__(self):
         super().__init__("c")
         self.type = "SERVER_FULL"
-    def parse(self,data,_):
+    def parse(self,data,raw):
         self.playercount = data[1]
-        self.data = data
+        self.data = raw
 class ServerLogin(BasePacket):
     def __init__(self):
         super().__init__("c")
         self.type = "SERVER_LOGIN"
-    def parse(self,data,_):
+    def parse(self,data,raw):
         self.playercount = data[1]
         self.challengestring = data[2]
         self.version = data[3]
-        self.data = data
+        self.data = raw
 class Accepted(BasePacket):
     def __init__(self):
         super().__init__("c")
         self.type = "ACCEPTED"
-    def parse(self,data,_):
-        self.data = data
+    def parse(self,data,raw):
+        self.data = raw
 class Rejected(BasePacket):
     def __init__(self):
         super().__init__("c")
         self.type = "REJECTED"
-    def parse(self,data,_):
-        self.data = data
+    def parse(self,data,raw):
+        self.data = raw
 class No_life_tokens(Rejected):
     def __init__(self):
         super().__init__()
         self.type = "REJECTED_NO_LIFE_TOKENS"
-    def parse(self,data,_):
-        self.data = data
+    def parse(self,data,raw):
+        self.data = raw
 class Login(BasePacket):
     def __init__(self):
         super().__init__("s")
         self.type = "CLIENT_LOGIN"
-    def parse(self,data,_):
+    def parse(self,data,raw):
         self.email,self.password_hash,self.account_key_hash,self.tutorial_number = data[1:5]
         self.twins = False
+        self.data = raw
         if len(data) > 5:
             self.twin_code_hash = data[5]
             self.twin_count = data[6]
@@ -95,7 +97,7 @@ class PlayerInfo(BasePacket):
         super().__init__("c")
         self.type = "PLAYERINFO"
     def parse(self,data,rawdata):
-        self.data = data
+        self.data = rawdata
         self.players = []
         for player in rawdata.split(b"\n")[1:]:
             self.players.append(player.split(b" "))
@@ -105,6 +107,7 @@ class PlayerSaid(BasePacket):
         self.type = "PLAYER_SAID"
     def parse(self,data,rawdata):
         tokens = rawdata.split("\n")[1:]
+        self.data = rawdata
         self.messages = []
         for token in tokens:
             self.messages.append(token.split(" "))
