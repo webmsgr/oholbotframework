7901c55655226c86389dc7f919381eaa45a42fcd
Wackery Smackery <zapcontact000@gmail.com>
2019-10-01 17:29:14 -0700

    it works! kind of

--- ohol.py
+++ ohol.py
@@ -1,12 +1,12 @@
 #! /usr/bin/env python3
 """A One Hour One Life decoder/encoder server, recives messages from the client and
 passes them to bot code that allows for packet injection and packet reading"""
-
+import traceback
 import select
 import oholparser as parser
 import socket
 import time
-
+import sys
 
 TIME_WAIT = 0.01
 BIND_ADDR = ''
@@ -21,45 +21,38 @@ def Route():
     listener.bind((BIND_ADDR, BIND_PORT))
     listener.listen(1)
     client, caddr = listener.accept()
+    print(caddr)
     listener.close()
     server = socket.socket()
     server.connect((SERV_ADDR, SERV_PORT))
     running = True
+    client.setblocking(1)
+    server.setblocking(1)
+    
     while running:
         try:
             rlist = select.select([client, server], [], [])[0]
             if client in rlist:
-                stream = b""
-                buf = b" "
-                while buf != b"":
-                    buf = client.recv(4096)
-                    stream += buf
-                buf = stream
-                myparser.parsepacket(buf.decode("ascii"))
+                buf = client.recv(4096)
                 if len(buf) == 0:
                     running = False
+                print("S <-- {}".format(buf))
+                myparser.parsepacket(buf)
                 server.send(buf)
+
             if server in rlist and running:
-                stream = b""
-                buf = b" "
-                while buf != b"":
-                    buf = server.recv(4096)
-                    stream += buf
-                buf = stream
-                myparser.parsepacket(buf.decode("ascii"))
+                buf = server.recv(4096)
                 if len(buf) == 0:
                     running = False
+                # Parse, modify, or halt traffic here
+                print("C <-- {}".format(buf))
+                myparser.parsepacket(buf)
                 client.send(buf)
-        except:
-            pass
-    try:
-        client.close()
-    except:
-        pass
-    try:
-        server.close()
-    except:
-        pass
+        except Exception as e:
+            traceback.print_exception(*sys.exc_info())
+            running = False
+    client.close()
+    server.close()
     return myparser.parsed
 
-
+packets = Route()
--- oholparser.py
+++ oholparser.py
@@ -3,13 +3,15 @@ class Parser():
     def __init__(self):
         self.parsed = []
     def parsepacket(self,packet):
-        packets = packet.split("#")
+        packets = packet.split(b"#")
         if packets[0] == packet:
-            packet = packet.split("\n")
-            packet = [x for x in packet if x.strip() != ""]
-            packetobj = packobj.get(packet[0],UnknownPacket)()
-            packetobj.parse(packet)
-            self.parsed.append(packetobj)
+            rawpacket = packet
+            packet = packet.split()
+            packet = [x for x in packet if x != b""]
+            if packet != []:
+                packetobj = packobj.get(packet[0].strip(),UnknownPacket)()
+                packetobj.parse(packet,rawpacket)
+                self.parsed.append(packetobj)
         else:
             [self.parsepacket(x) for x in packets if x.strip() != ""]
 class BasePacket:
@@ -24,7 +26,7 @@ class UnknownPacket(BasePacket):
     def __init__(self):
         super().__init__("?")
         self.type = "UNKNOWN"
-    def parse(self,args):
+    def parse(self,args,_):
         self.data = args
 
 class Frame(BasePacket):
@@ -37,21 +39,21 @@ class Shutdown(BasePacket):
     def __init__(self):
         super().__init__("c")
         self.type = "SHUTDOWN"
-    def parse(self,data):
+    def parse(self,data,_):
         self.playercount = data[1]
         self.data = data
 class Server_full(BasePacket):
     def __init__(self):
         super().__init__("c")
         self.type = "SERVER_FULL"
-    def parse(self,data):
+    def parse(self,data,_):
         self.playercount = data[1]
         self.data = data
 class ServerLogin(BasePacket):
     def __init__(self):
         super().__init__("c")
         self.type = "SERVER_LOGIN"
-    def parse(self,data):
+    def parse(self,data,_):
         self.playercount = data[1]
         self.challengestring = data[2]
         self.version = data[3]
@@ -60,39 +62,57 @@ class Accepted(BasePacket):
     def __init__(self):
         super().__init__("c")
         self.type = "ACCEPTED"
-    def parse(self,data):
+    def parse(self,data,_):
         self.data = data
 class Rejected(BasePacket):
     def __init__(self):
         super().__init__("c")
         self.type = "REJECTED"
-    def parse(self,data):
+    def parse(self,data,_):
         self.data = data
 class No_life_tokens(Rejected):
     def __init__(self):
         super().__init__()
         self.type = "REJECTED_NO_LIFE_TOKENS"
-    def parse(self,data):
+    def parse(self,data,_):
         self.data = data
 class Login(BasePacket):
     def __init__(self):
         super().__init__("s")
         self.type = "CLIENT_LOGIN"
-    def parse(self,data):
-        data = data[1].split(" ")
+    def parse(self,data,_):
         self.email,self.password_hash,self.account_key_hash,self.tutorial_number = data[1:5]
         self.twins = False
         if len(data) > 5:
             self.twin_code_hash = data[5]
             self.twin_count = data[6]
             self.twins = True
+class PlayerInfo(BasePacket):
+    def __init__(self):
+        super().__init__("c")
+        self.type = "PLAYERINFO"
+    def parse(self,data,rawdata):
+        self.data = data
+        self.players = []
+        for player in rawdata.split(b"\n")[1:]:
+            self.players.append(player.split(b" "))
+class PlayerSaid(BasePacket):
+    def __init__(self):
+        super().__init__("c")
+        self.type = "PLAYER_SAID"
+    def parse(self,data,rawdata):
+        tokens = rawdata.split("\n")[1:]
+        self.messages = []
+        for token in tokens:
+            self.messages.append(token.split(" "))
 
-packobj = {"FM":Frame,
-            "SHUTDOWN":Shutdown,
-            "SERVER_FULL":Server_full,
-            "SN":ServerLogin,
-            "ACCEPTED":Accepted,
-            "REJECTED":Rejected,
-            "NO_LIFE_TOKENS":No_life_tokens,
-            "LOGIN": Login
+packobj = {b"FM":Frame,
+            b"SHUTDOWN":Shutdown,
+            b"SERVER_FULL":Server_full,
+            b"SN":ServerLogin,
+            b"ACCEPTED":Accepted,
+            b"REJECTED":Rejected,
+            b"NO_LIFE_TOKENS":No_life_tokens,
+            b"LOGIN": Login,
+            b"PU": PlayerInfo
             }
