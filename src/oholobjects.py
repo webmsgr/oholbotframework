18ea1959da8dae4b689fc36c582fa9bda04856fd
Wackery <zapcontact000@gmail.com>
2019-10-07 16:55:09 +0000

    add transitions

--- src/oholobjects.py
+++ src/oholobjects.py
@@ -38,6 +38,12 @@ class OHOLObjects():
         self.nameid = {}
         for obj in self.objs:
             self.nameid[sname(self.objs[obj].name)] = self.objs[obj].id
+        self.nameid["time"] = "-1"
+        self.objs["-1"] = OHOLObject({"name":"Time","id":"-1"})
+        self.nameid["hand"] = "0"
+        self.objs["0"] = OHOLObject({"name":"Hand","id":"0"})
+        self.nameid["empty_hand"] = "0"
+        self.objs["-2"] = OHOLObject({"name":"Empty Hand","id":"-2"})
     def __getattr__(self,name):
         if name in self.nameid:
             return self.objs[self.nameid[name]]
@@ -60,15 +66,56 @@ class OHOLObjects():
     def __iter__(self):
         return iter(self.objs)
 
+class OHOLTransition:
+    def __init__(self,data,obj):
+        self.props = data
+        self.obj = obj
+    def __getattr__(self,prop):
+        if prop in self.props:
+            return self.props[prop]
+        else:
+            raise AttributeError("No property named {}".format(prop))
+    def __hash__(self):
+        return hash(self.props)
+    def __repr__(self):
+        tmp = (self.obj.byid(m) for m in (self.actor,self.target,self.newActor,self.newTarget))
+        return "<OHOL Transition {} + {} = {} + {}>".format(*tmp)
+    def __getstate__(self):
+        return self.props
+    def __setstate__(self,data):
+        self.props = data
+    def __getitem__(self, item):
+        return self.props[item]
+    def __iter__(self):
+        return iter(self.props)
+
+class OHOLTransitions:
+    def __init__(self,obj=None):
+        trans = parseTransitions()
+        if obj is None:
+            obj = OHOLObjects()
+        self.trans = {}
+        self.obj = obj
+        for tran in trans:
+            self.trans[tran] = OHOLTransition(trans[tran],obj)
+    def __repr__(self):
+        return "<All OHOL Transitions>"
+    def __getstate__(self):
+        return self.trans
+    def __setstate__(self,data):
+        self.trans = data
+    def __getitem__(self, item):
+        return self.trans[item]
+    def __iter__(self):
+        return iter(self.trans)
+
 
 def graball(trans=None,obj=None):
-    out = [trans,obj]
-    if trans is None:
-        out[0] = parseTransitions()
     if obj is None:
-        out[1] = parseObjects()
-    return out
-
+        obj = OHOLObjects()
+    if trans is None:
+        trans = OHOLTransitions(obj)
+    return (obj,trans)
 
 
 def parseTransitions():
@@ -83,6 +130,8 @@ def parseTransitions():
         elif "_L" in transition:
             useLastTarget = True
         actor,target = os.path.basename(transition).split(".")[0].split("_")[:2]
+        if target == "-2":
+            continue
         thisobj["actor"] = actor
         thisobj["target"] = target
         with open(transition) as fl:
